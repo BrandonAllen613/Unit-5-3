8b5ac0df46b06d5c2a6b75b92fb3a4f42df32b3f
BrandonAllen4 <>
2021-05-02 19:02:44 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+<!DOCTYPEhtml>
+<!-- This a comment -->
+<html lang="en-ca"/html>
+  <head>
+  <body>
+      <body style= "background-colour:yellow">
+      <input type="button" value="Click Here!" onclick="alert (Please Consider The Following Actions)"/>
+       <button type="button">Click Here!</button>
+       document.getElementById("myText").value = "Brandon Allen";
+
